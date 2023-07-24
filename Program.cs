947f43b1057d1bebc27952afa4cb1184c59d9cd5
AgentSergei <sergei.agent888@gmail.com>
2023-07-24 16:44:59 +0300

    task36

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*/
+//
 
 Console.Write("Введите количество элементов массива: ");
 int a = Convert.ToInt32(Console.ReadLine());
@@ -32,3 +32,39 @@ mas(a);
 Console.Write("\nКоличество чётных чисел в массиве:"  + kol(randomArray));
 
 
+*/
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void mas(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + " ");
+}
+
+}
+
+int kol(int[] randomArray)
+{
+int sum = 0;
+int i = 0;
+while (i < randomArray.Length)
+{
+sum = sum + randomArray[i];
+i = i + 2;
+}
+return sum;
+}
+
+mas(a);
+Console.Write("\nCумма элементов, стоящих на нечётных позициях: " + kol(randomArray));
