416885ec049809573353d59ea6a37ba2e69de04c
AgentSergei <sergei.agent888@gmail.com>
2023-07-24 17:32:22 +0300

    task38

--- Program.cs
+++ Program.cs
@@ -70,3 +70,25 @@ mas(a);
 Console.Write("\nCумма элементов, стоящих на нечётных позициях: " + kol(randomArray));
 
 */
+
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+  
+ Console.WriteLine("Введите размер массива:");
+int n = Convert.ToInt32(Console.ReadLine()); // Задаём размер массива
+int [] mass  = new int[n];      // Создаём массив
+for (int i = 0; i < n; i++)
+  { Console.Write($" Введите {i}й элемент: "); mass[i]= Convert.ToInt32(Console.ReadLine()); }
+int min = 0;
+int max = 0;
+foreach (int i in mass)  // Поиск максимального и минимального значения
+{
+    if (min > i) min = i;
+    if (max < i) max = i;
+}
+Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
+
